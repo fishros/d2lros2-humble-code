9af07f40246d534980cd5b6ed7ef016cd79f5a89
鱼香ROS <87068644+fishros@users.noreply.github.com>
2022-07-02 02:52:28 +0800

    [feat]:增加建图和导航代码

--- chapt10/chapt10_ws/src/fishbot_cartographer/config/fishbot_2d.lua
+++ chapt10/chapt10_ws/src/fishbot_cartographer/config/fishbot_2d.lua
@@ -6,15 +6,18 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_link",
-  published_frame = "odom",
+  tracking_frame = "laser_frame",
+  published_frame = "laser_frame",
+  -- tracking_frame = "horizontal_laser_link",
+  -- published_frame = "horizontal_laser_link",
   odom_frame = "odom",
   -- true改为false，不用提供里程计数据
-  provide_odom_frame = false,
+  provide_odom_frame = true,
   -- false改为true，仅发布2D位资
-  publish_frame_projected_to_2d = true,
+  publish_frame_projected_to_2d = false,
+  use_pose_extrapolator = true,
   -- false改为true，使用里程计数据
-  use_odometry = true,
+  use_odometry = false,
   use_nav_sat = false,
   use_landmarks = false,
   -- 0改为1,使用一个雷达
@@ -42,7 +45,7 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 -- 0改成0.10,比机器人半径小的都忽略
 TRAJECTORY_BUILDER_2D.min_range = 0.10
 -- 30改成3.5,限制在雷达最大扫描范围内，越小一般越精确些
-TRAJECTORY_BUILDER_2D.max_range = 3.5
+TRAJECTORY_BUILDER_2D.max_range = 5.5
 -- 5改成3,传感器数据超出有效范围最大值
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
 -- true改成false,不使用IMU数据，大家可以开启，然后对比下效果
@@ -50,12 +53,19 @@ TRAJECTORY_BUILDER_2D.use_imu_data = false
 -- false改成true,使用实时回环检测来进行前端的扫描匹配
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true 
 -- 1.0改成0.1,提高对运动的敏感度
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
+-- TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
 -- 0.55改成0.65,Fast csm的最低分数，高于此分数才进行优化。
 POSE_GRAPH.constraint_builder.min_score = 0.65
 --0.6改成0.7,全局定位最小分数，低于此分数则认为目前全局定位不准确
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e-1
+
+POSE_GRAPH.optimization_problem.huber_scale = 1e2
+POSE_GRAPH.optimize_every_n_nodes = 35
 
 -- 设置0可关闭全局SLAM
 -- POSE_GRAPH.optimize_every_n_nodes = 0
